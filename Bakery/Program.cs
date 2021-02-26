044812b2c66b3466e2f78cffee96f7b8ee0f7873
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-26 10:57:47 -0800

    finish base functionality for Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Bakery.Models;
 
 namespace Bakery
 {
@@ -6,6 +7,8 @@ namespace Bakery
   {
     static void Main()
     {
+      int loaves = 0; int pastries = 0;
+      Console.WriteLine("");
       Console.WriteLine("\\o/ \\o/ Welcome to Pierre's Bakery! \\o/ \\o/");
       Console.WriteLine("      These are our prices for today.");
       Console.WriteLine("");
@@ -22,18 +25,58 @@ namespace Bakery
       Console.WriteLine("Please enter the number of loaves you would like.");
       try
       {
-        int loaves = Int32.Parse(Console.ReadLine());
+        loaves = Int32.Parse(Console.ReadLine());
         if(loaves < 0)
         {
-          throw new Exception("Negative amount"); // inspo from https://www.dotnetperls.com/throw
+          throw new ArgumentException("Negative amount detected. Please enter a positive integer."); // inspo from https://www.dotnetperls.com/throw && https://www.tutorialsteacher.com/csharp/csharp-exception
         }
       }
       catch (FormatException) // inspo from https://docs.microsoft.com/en-us/dotnet/api/system.int32.parse?view=net-5.0
       {
-          Console.WriteLine("Non-integer detected. Please enter a positive integer.");
-          Console.WriteLine("");
+        Console.WriteLine("Non-integer detected. Please enter a positive integer.");
+        Main();
+      }
+      catch(OverflowException)
+      {
+        Console.WriteLine("Overflow detected. Please enter a smaller positive integer (<=2,147,483,647");
+        Main();
+      }
+      catch (ArgumentException ex)
+      {
+        Console.WriteLine(ex.Message); // https://docs.microsoft.com/en-us/dotnet/api/system.argumentexception.message?view=netframework-4.8#System_ArgumentException_Message
+        Main();
       }
 
+      Console.WriteLine("Please enter the number of pastries you would like.");
+      try
+      {
+        pastries = Int32.Parse(Console.ReadLine());
+        if(pastries < 0)
+        {
+          throw new ArgumentException("Negative amount detected. Please enter a positive integer."); // inspo from https://www.dotnetperls.com/throw && https://www.tutorialsteacher.com/csharp/csharp-exception
+        }
+      }
+      catch (FormatException) // inspo from https://docs.microsoft.com/en-us/dotnet/api/system.int32.parse?view=net-5.0
+      {
+        Console.WriteLine("Non-integer detected. Please enter a positive integer.");
+        Main();
+      }
+      catch(OverflowException)
+      {
+        Console.WriteLine("Overflow detected. Please enter a smaller positive integer (<=2,147,483,647");
+        Main();
+      }
+      catch (ArgumentException ex)
+      {
+        Console.WriteLine(ex.Message); // https://docs.microsoft.com/en-us/dotnet/api/system.argumentexception.message?view=netframework-4.8#System_ArgumentException_Message
+        Main();
+      }
+
+      Bread bread = new Bread();
+      Pastry pastry = new Pastry();
+      int total = bread.Price(loaves) + pastry.Price(pastries);
+      Console.WriteLine($"Your total amount is ${total}");
+      Main();
     }
   }
 
