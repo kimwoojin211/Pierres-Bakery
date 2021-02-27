63c1d470877ddbb356bdd57bbf644e3ccffbc2f1
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-26 16:42:18 -0800

    allow user to indefinitely select and add until total is entered

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -22,26 +22,51 @@ namespace Bakery
       Console.WriteLine("SUPERDEAL   Pastries: 1 for $2      SUPERDEAL");
       Console.WriteLine("SUPERDEAL             3 for $5      SUPERDEAL");
       Console.WriteLine("SUPERDEAL                           SUPERDEAL");
+      Console.WriteLine("SUPERDEAL    Any 5 Items:  -$2!     SUPERDEAL");
+      Console.WriteLine("SUPERDEAL                           SUPERDEAL");
       Console.WriteLine("SUPERDEALSUPERDEALSUPERDEALSUPERDEALSUPERDEAL");
       Console.WriteLine("");
 
-      int breadAmount = -1, pastryAmount = -1;
-      Console.WriteLine("Please enter the number of loaves of bread you would like. (Positive integers only.)");
-      while(breadAmount < 0)
+      int breadAmount, pastryAmount;
+      int totalBreadAmount = 0, totalPastryAmount = 0;
+      string choice = "";
+      while(choice != "TOTAL")
       {
-        breadAmount = BakedGood.NumberOf(Console.ReadLine());
+        Console.WriteLine("Enter \"Bread\" or \"Pastry\" (Case Insensitive), or enter \"Total\" to calculate the total cost of your order)");
+        choice = Console.ReadLine().ToUpper();
+        switch(choice)
+        {
+          case "BREAD":
+            breadAmount = -1;
+            Console.WriteLine("Please enter the number of loaves of bread you would like. (Positive integers only.)");
+            while(breadAmount < 0)
+            {
+              breadAmount = BakedGood.NumberOf(Console.ReadLine());
+            }
+            totalBreadAmount += breadAmount;
+            break;
+          case "PASTRY":
+            pastryAmount = -1;
+            Console.WriteLine("Please enter the number of pastries you would like. (Positive integers only.)");
+            while(pastryAmount < 0)
+            {
+              pastryAmount = BakedGood.NumberOf(Console.ReadLine());
+            }
+            totalPastryAmount += pastryAmount;
+            break;
+          case "TOTAL":
+            break;
+          default:
+            Console.WriteLine("Invalid selection. Please enter Bread, Pastry, or Total.");
+            break;
+        }
       }
-      Console.WriteLine("Please enter the number of pastries you would like. (Positive integers only.)");
-      while(pastryAmount < 0)
-      {
-        pastryAmount = BakedGood.NumberOf(Console.ReadLine());
-      }
-      Bread bread = new Bread(breadAmount);
-      Pastry pastry = new Pastry(pastryAmount);
+      Bread bread = new Bread(totalBreadAmount);
+      Pastry pastry = new Pastry(totalPastryAmount);
       int totalBreadPrice = bread.Price();
       int totalPastryPrice = pastry.Price();
-
-      Console.WriteLine($"Your total amount is ${totalBreadPrice + totalPastryPrice}");
+      int fiveForThree = 2*((totalBreadAmount+totalPastryAmount)/5);
+      Console.WriteLine($"Your total of {totalBreadAmount} loaves and {totalPastryAmount} pastries comes out to ${totalBreadPrice + totalPastryPrice - fiveForThree}");
     }
   }
 }
